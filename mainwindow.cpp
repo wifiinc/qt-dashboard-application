1eef87f99c5e8cb1388094d421e0f6cf2b209f6f
scxtyy <akt1238karim@gmail.com>
2025-05-03 17:12:07 +0200

    GUI opzet (dummy version)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,70 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    // Sliders
+    ui->sliderR->setRange(0, 255);
+    ui->sliderG->setRange(0, 255);
+    ui->sliderB->setRange(0, 255);
+
+    // Dummy sensorwaarden
+    ui->lcdTemp->display(21.3);
+    ui->lcdCO2->display(580);
+    ui->lcdHumidity->display(42);
+    ui->labelCurrentMenu->setText("-");
+    ui->labelVentilatorStatus->setText("Uit");
+    ui->labelLichtkrant->setText("Welkom");
+    ui->labelTafelStatus->setText("Tafel staat UIT");
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::on_btnSetColor_clicked() {
+    if (!ui->checkBoxLightOn->isChecked()) {
+        applyLightColor(0, 0, 0);
+        return;
+    }
+    int r = ui->sliderR->value();
+    int g = ui->sliderG->value();
+    int b = ui->sliderB->value();
+    applyLightColor(r, g, b);
+}
+
+void MainWindow::applyLightColor(int r, int g, int b) {
+    qDebug() << "RGB ingesteld op R:" << r << " G:" << g << " B:" << b;
+}
+
+void MainWindow::on_btnPublishMenu_clicked() {
+    QString menu = ui->textEditMenuInput->toPlainText();
+    ui->labelCurrentMenu->setText(menu);
+}
+
+void MainWindow::on_btnVentilatorAan_clicked() {
+    ui->labelVentilatorStatus->setText("Aan");
+    qDebug() << "Ventilator aan";
+}
+
+void MainWindow::on_btnVentilatorUit_clicked() {
+    ui->labelVentilatorStatus->setText("Uit");
+    qDebug() << "Ventilator uit";
+}
+
+void MainWindow::on_btnTafelToggle_clicked() {
+    QString current = ui->labelTafelStatus->text();
+    if (current.contains("UIT")) {
+        ui->labelTafelStatus->setText("Tafel staat AAN");
+    } else {
+        ui->labelTafelStatus->setText("Tafel staat UIT");
+    }
+}
+
+void MainWindow::on_btnUpdateLichtkrant_clicked() {
+    QString tekst = ui->lineEditLichtkrant->text();
+    ui->labelLichtkrant->setText(tekst);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,20 +4,28 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_btnSetColor_clicked();
+    void on_btnPublishMenu_clicked();
+    void on_btnVentilatorAan_clicked();
+    void on_btnVentilatorUit_clicked();
+    void on_btnTafelToggle_clicked();
+    void on_btnUpdateLichtkrant_clicked();
 
 private:
     Ui::MainWindow *ui;
+
+    void applyLightColor(int r, int g, int b);
 };
+
 #endif // MAINWINDOW_H
