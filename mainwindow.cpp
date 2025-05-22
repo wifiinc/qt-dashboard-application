860eb7e4d4878d36a3a1b656ac509727bdff4f51
scxtyy <akt1238karim@gmail.com>
2025-05-22 08:48:24 +0200

    GUI elements lichtkrant

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,265 +1,28 @@
 #include "mainwindow.h"
-
-#include <QColorDialog>
-#include <QDebug>
-#include <QSettings>
-
 #include "./ui_mainwindow.h"
-
-extern "C" {
-#include "packets.h"
-}
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+    setWindowTitle("Lichtkrant Dashboard");
 
-    : QMainWindow(parent), ui(new Ui::MainWindow), client("127.0.0.1", 5000) {
-  QSettings settings(QSettings::IniFormat, QSettings::UserScope,
-                     "LBAutomatisatie", "DashboardApp");
-
-  qDebug() << settings.fileName();
-
-  loadSettings();
-  writeSettings();
-
-  ui->setupUi(this);
-
-  requestluisteren();
-
-  this->setWindowTitle("L&B Automatisatie Dashboard");
-  this->setWindowIcon(QIcon(":/icon.png"));
-
-  QAction* settingsAction = new QAction("Instellingen", this);
-  connect(settingsAction, &QAction::triggered, this,
-          &MainWindow::on_btnOpenSettings_clicked);
-
-  ui->menubar->addAction(settingsAction);
-
-  settingsWindow = new SettingsWindow(this);
-
-  connect(settingsWindow, &SettingsWindow::rgbSensorIdChanged, this,
-          &MainWindow::updateRgbSensorId);
-  connect(settingsWindow, &SettingsWindow::bridgeIpChanged, this,
-          &MainWindow::updateBridgeIp);
-  connect(settingsWindow, &SettingsWindow::tafelSensorIdChanged, this,
-          &MainWindow::updateTafelSensorId);
-  connect(settingsWindow, &SettingsWindow::updateSensorList, this,
-          &MainWindow::setSensorList);
-
-  QString stylesheet =
-      QString("background-color: %1;").arg(huidigekleurRGBLed.name());
-  ui->rgbLightColorWidget->setStyleSheet(stylesheet);
+    connect(ui->btnVerzendLichtkrant, &QPushButton::clicked,
+            this, &MainWindow::on_btnVerzendLichtkrant_clicked);
 }
 
 MainWindow::~MainWindow() {
-  delete settingsWindow;
-  delete ui;
+    delete ui;
 }
 
-void MainWindow::setSensorList()
-{
-    client.setSensorList({
-        {SensorType::LIGHT, rgbSensorId},
-        {SensorType::RGB_LIGHT, rgbSensorId},
-        {SensorType::LIGHT, tafel1SensorId},
-        {SensorType::LIGHT, tafel2SensorId},
-        {SensorType::LIGHT, tafel3SensorId},
-    });
-}
+void MainWindow::on_btnVerzendLichtkrant_clicked() {
+    QString boodschap = ui->lineEditLichtkrant->text().trimmed();
+    if (boodschap.isEmpty()) {
+        qDebug() << "Geen tekst ingevoerd voor lichtkrant.";
+        return;
+    }
 
-void MainWindow::writeSettings() {
-  QSettings settings(QSettings::IniFormat, QSettings::UserScope,
-                     "LBAutomatisatie", "DashboardApp");
+    qDebug() << "Lichtkrant boodschap verzenden:" << boodschap;
 
-  settings.beginGroup("Connection");
-  settings.setValue("bridgeIp", bridgeIp);
-  settings.setValue("bridgePort", bridgePort);
-  settings.endGroup();
 
-  settings.beginGroup("Sensors");
-  settings.setValue("rgbSensorId", rgbSensorId);
-  settings.setValue("tafel1SensorId", tafel1SensorId);
-  settings.setValue("tafel2SensorId", tafel2SensorId);
-  settings.setValue("tafel3SensorId", tafel3SensorId);
-  settings.endGroup();
-}
-
-void MainWindow::loadSettings() {
-  QSettings settings(QSettings::IniFormat, QSettings::UserScope,
-                     "LBAutomatisatie", "DashboardApp");
-
-  settings.beginGroup("Connection");
-  bridgeIp = settings.value("bridgeIp", "127.0.0.1").toString();
-  bridgePort = settings.value("bridgePort", 5000).toInt();
-  settings.endGroup();
-
-  settings.beginGroup("Sensors");
-  rgbSensorId = settings.value("rgbSensorId", 1).toInt();
-  tafel1SensorId = settings.value("tafel1SensorId", 1).toInt();
-  tafel2SensorId = settings.value("tafel2SensorId", 1).toInt();
-  tafel3SensorId = settings.value("tafel3SensorId", 1).toInt();
-  settings.endGroup();
-}
-
-void MainWindow::requestluisteren() {
-  client.connectToServer();
-  connect(&client, &Tcpsocket::packetReceived, [](const sensor_packet& packet) {
-    qDebug() << "Packet ontvangen van sensor ID:"
-             << packet.data.generic.metadata.sensor_id;
-  });
-
-  connect(
-      &client, &Tcpsocket::packetReceived, [this](const sensor_packet& packet) {
-        if (packet.data.generic.metadata.sensor_type == SensorType::LIGHT) {
-          if (packet.data.generic.metadata.sensor_id == tafel1SensorId) {
-            tafel1State = packet.data.light.target_state == 1 ? true : false;
-            ui->tafel1Status->setText(tafel1State ? "Tafel staat AAN"
-                                                  : "Tafel staat UIT");
-          }
-        }
-      });
-
-  connect(
-      &client, &Tcpsocket::packetReceived, [this](const sensor_packet& packet) {
-        if (packet.data.generic.metadata.sensor_type == SensorType::LIGHT) {
-          if (packet.data.generic.metadata.sensor_id == tafel2SensorId) {
-            tafel2State = packet.data.light.target_state == 1 ? true : false;
-            ui->tafel2Status->setText(tafel2State ? "Tafel staat AAN"
-                                                  : "Tafel staat UIT");
-          }
-        }
-      });
-
-  connect(
-      &client, &Tcpsocket::packetReceived, [this](const sensor_packet& packet) {
-        if (packet.data.generic.metadata.sensor_type == SensorType::LIGHT) {
-          if (packet.data.generic.metadata.sensor_id == tafel3SensorId) {
-            tafel3State = packet.data.light.target_state == 1 ? true : false;
-            ui->tafel3Status->setText(tafel3State ? "Tafel staat AAN"
-                                                  : "Tafel staat UIT");
-          }
-        }
-      });
-}
-
-void MainWindow::on_btnOpenSettings_clicked() {
-  settingsWindow->setRgbSensorId(rgbSensorId);
-
-  settingsWindow->setBridgeIp(bridgeIp);
-  settingsWindow->setBridgePort(bridgePort);
-  settingsWindow->setTafelSensorId(tafel1SensorId, tafel2SensorId,
-                                   tafel3SensorId);
-
-  settingsWindow->show();
-}
-
-void MainWindow::updateRgbSensorId(int newID) {
-  qDebug() << "Sensor ID aangepast naar:" << newID;
-  rgbSensorId = newID;
-
-  writeSettings();
-}
-
-void MainWindow::updateBridgeIp(const QString& newIp, int newPort) {
-  bridgeIp = newIp;
-  bridgePort = newPort;
-
-  client.updateConnection(newIp, newPort);
-  qDebug() << "Bridge IP aangepast naar:" << bridgeIp;
-  qDebug() << "Bridge Port aangepast naar:" << bridgePort;
-
-  writeSettings();
-}
-
-void MainWindow::updateTafelSensorId(int tafel1, int tafel2, int tafel3) {
-  tafel1SensorId = tafel1;
-  tafel2SensorId = tafel2;
-  tafel3SensorId = tafel3;
-
-  writeSettings();
-}
-
-void MainWindow::on_changeRGBBtn_clicked() {
-  huidigekleurRGBLed = QColorDialog::getColor(huidigekleurRGBLed, this,
-                                              "Instellen kleur RGB led");
-  QString stylesheet =
-      QString("background-color: %1;").arg(huidigekleurRGBLed.name());
-  ui->rgbLightColorWidget->setStyleSheet(stylesheet);
-}
-
-void MainWindow::on_saveRGBBtn_clicked() {
-  int r = 0, g = 0, b = 0;
-
-  r = huidigekleurRGBLed.red();
-  g = huidigekleurRGBLed.green();
-  b = huidigekleurRGBLed.blue();
-
-  qDebug() << "RGB ingesteld op R:" << r << " G:" << g << " B:" << b;
-
-  sensor_packet pakket;
-  pakket.header.ptype = PacketType::DASHBOARD_POST;
-  pakket.header.length = sizeof(sensor_packet_rgb_light);
-
-  pakket.data.rgb_light.metadata.sensor_type = SensorType::RGB_LIGHT;
-  pakket.data.rgb_light.metadata.sensor_id = static_cast<uint8_t>(rgbSensorId);
-  pakket.data.rgb_light.red_state = static_cast<uint8_t>(r);
-  pakket.data.rgb_light.green_state = static_cast<uint8_t>(g);
-  pakket.data.rgb_light.blue_state = static_cast<uint8_t>(b);
-
-  client.sendPacket(pakket);
-}
-
-void MainWindow::on_checkBoxLightOn_toggled(bool checked) {
-  sensor_packet pakket;
-  pakket.header.ptype = PacketType::DASHBOARD_POST;
-  pakket.header.length = sizeof(sensor_packet_light);
-  pakket.data.light.metadata.sensor_type = SensorType::LIGHT;
-  pakket.data.light.metadata.sensor_id = static_cast<uint8_t>(rgbSensorId);
-  pakket.data.light.target_state = checked ? 1 : 0;
-
-  client.sendPacket(pakket);
-}
-
-void MainWindow::on_tafel1Toggle_clicked() {
-  tafel1State = !tafel1State;
-  ui->tafel1Status->setText(tafel1State ? "Tafel staat AAN"
-                                        : "Tafel staat UIT");
-
-  sensor_packet pakket;
-  pakket.header.ptype = PacketType::DASHBOARD_POST;
-  pakket.header.length = sizeof(sensor_packet_light);
-  pakket.data.light.metadata.sensor_type = SensorType::LIGHT;
-  pakket.data.light.metadata.sensor_id = static_cast<uint8_t>(tafel1SensorId);
-  pakket.data.light.target_state = tafel1State ? 1 : 0;
-
-  client.sendPacket(pakket);
-}
-
-void MainWindow::on_tafel2Toggle_clicked() {
-  tafel2State = !tafel2State;
-  ui->tafel2Status->setText(tafel2State ? "Tafel staat AAN"
-                                        : "Tafel staat UIT");
-
-  sensor_packet pakket;
-  pakket.header.ptype = PacketType::DASHBOARD_POST;
-  pakket.header.length = sizeof(sensor_packet_light);
-  pakket.data.light.metadata.sensor_type = SensorType::LIGHT;
-  pakket.data.light.metadata.sensor_id = static_cast<uint8_t>(tafel2SensorId);
-  pakket.data.light.target_state = tafel2State ? 1 : 0;
-
-  client.sendPacket(pakket);
-}
-
-void MainWindow::on_tafel3Toggle_clicked() {
-  tafel3State = !tafel3State;
-  ui->tafel3Status->setText(tafel3State ? "Tafel staat AAN"
-                                        : "Tafel staat UIT");
-
-  sensor_packet pakket;
-  pakket.header.ptype = PacketType::DASHBOARD_POST;
-  pakket.header.length = sizeof(sensor_packet_light);
-  pakket.data.light.metadata.sensor_type = SensorType::LIGHT;
-  pakket.data.light.metadata.sensor_id = static_cast<uint8_t>(tafel3SensorId);
-  pakket.data.light.target_state = tafel3State ? 1 : 0;
-
-  client.sendPacket(pakket);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QSettings>
-
-#include "Tcpsocket.h"
-#include "settingswindow.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -14,46 +10,17 @@ class MainWindow;
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow {
-  Q_OBJECT
+    Q_OBJECT
 
- public:
-  explicit MainWindow(QWidget* parent = nullptr);
-  ~MainWindow();
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
 
- private slots:
-  void setSensorList();
-  void writeSettings();
-  void loadSettings();
-  void requestluisteren();
-  void on_btnOpenSettings_clicked();
-  void updateRgbSensorId(int newID);
-  void updateBridgeIp(const QString& newIp, int newPort);
-  void updateTafelSensorId(int tafel1, int tafel2, int tafel3);
+private slots:
+    void on_btnVerzendLichtkrant_clicked();
 
-  void on_changeRGBBtn_clicked();
-  void on_saveRGBBtn_clicked();
-  void on_checkBoxLightOn_toggled(bool checked);
-
-  void on_tafel1Toggle_clicked();
-  void on_tafel2Toggle_clicked();
-  void on_tafel3Toggle_clicked();
-
- private:
-  Ui::MainWindow* ui;
-  Tcpsocket client;
-  SettingsWindow* settingsWindow;
-
-  int rgbSensorId;
-  QString bridgeIp;
-  int bridgePort;
-  int tafel1SensorId;
-  int tafel2SensorId;
-  int tafel3SensorId;
-
-  QColor huidigekleurRGBLed = Qt::white;
-  bool tafel1State = false;
-  bool tafel2State = false;
-  bool tafel3State = false;
+private:
+    Ui::MainWindow* ui;
 };
 
 #endif  // MAINWINDOW_H
