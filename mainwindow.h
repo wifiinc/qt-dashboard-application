e47378a4c38def0a7380e624a4e5d91e732f4a4a
scxtyy <akt1238karim@gmail.com>
2025-06-05 15:53:53 +0200

    uuh alles weer toegevoegd :(

--- mainwindow.h
+++ mainwindow.h
@@ -2,8 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QColor>
+#include <QSettings>
+
 #include "Tcpsocket.h"
+#include "settingswindow.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -15,15 +17,45 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
 private slots:
+    void setSensorList();
+    void writeSettings();
+    void loadSettings();
+    void requestluisteren();
+    void on_btnOpenSettings_clicked();
+    void updateRgbSensorId(int newID);
+    void updateBridgeIp(const QString& newIp, int newPort);
+    void updateTafelSensorId(int tafel1, int tafel2, int tafel3);
+
+    void on_changeRGBBtn_clicked();
+    void on_saveRGBBtn_clicked();
+    void on_checkBoxLightOn_toggled(bool checked);
+
+    void on_tafel1Toggle_clicked();
+    void on_tafel2Toggle_clicked();
+    void on_tafel3Toggle_clicked();
+
     void on_btnVerzendLichtkrant_clicked();
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow* ui;
     Tcpsocket client;
+    SettingsWindow* settingsWindow;
+
+    int rgbSensorId;
+    QString bridgeIp;
+    int bridgePort;
+    int tafel1SensorId;
+    int tafel2SensorId;
+    int tafel3SensorId;
+
+    QColor huidigekleurRGBLed = Qt::white;
+    bool tafel1State = false;
+    bool tafel2State = false;
+    bool tafel3State = false;
 };
 
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
