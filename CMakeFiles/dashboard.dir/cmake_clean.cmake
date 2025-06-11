1f18e17d9c9dcb60e4d2d7ffacb20b78b0cc8c8c
Daan Breur <git@daanbreur.systems>
2025-06-11 15:30:10 +0200

    asdasdsad

---
+++ CMakeFiles/dashboard.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dashboard_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/dashboard_autogen.dir/ParseCache.txt"
+  "dashboard_autogen"
+  "CMakeFiles/dashboard.dir/Tcpsocket.cpp.o"
+  "CMakeFiles/dashboard.dir/Tcpsocket.cpp.o.d"
+  "CMakeFiles/dashboard.dir/dashboard_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/dashboard.dir/dashboard_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/dashboard.dir/main.cpp.o"
+  "CMakeFiles/dashboard.dir/main.cpp.o.d"
+  "CMakeFiles/dashboard.dir/mainwindow.cpp.o"
+  "CMakeFiles/dashboard.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/dashboard.dir/settingswindow.cpp.o"
+  "CMakeFiles/dashboard.dir/settingswindow.cpp.o.d"
+  "dashboard"
+  "dashboard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dashboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/dashboard_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dashboard_autogen"
+  "dashboard_autogen/include/ui_mainwindow.h"
+  "dashboard_autogen/include/ui_settingswindow.h"
+  "dashboard_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/dashboard_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
